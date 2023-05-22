9b9f005171aa01b83deca306f1d8fd73a7d35959
holomekc <30546982+holomekc@users.noreply.github.com>
2023-05-22 05:29:39 +0200

    remove warn because of seq. @JsonIgnore in case Hibernate is working and response is generated.

--- hibernate-reactive-panache-quickstart/src/main/java/org/acme/hibernate/orm/panache/FruitBasket.java
+++ hibernate-reactive-panache-quickstart/src/main/java/org/acme/hibernate/orm/panache/FruitBasket.java
@@ -1,5 +1,6 @@
 package org.acme.hibernate.orm.panache;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 import jakarta.persistence.*;
 
@@ -12,6 +13,8 @@ public class FruitBasket extends PanacheEntity {
     @Column
     public String name;
 
+    // Just in case you get it running, to prevent issues with api json gen (cycle).
+    @JsonIgnore
     @OneToMany(mappedBy = "basket", fetch = FetchType.LAZY)
     public Collection<Fruit> fruits;
 }
--- hibernate-reactive-panache-quickstart/src/main/resources/import.sql
+++ hibernate-reactive-panache-quickstart/src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO fruit_basket(id, name) VALUES (1, 'MyBasket');
-ALTER SEQUENCE fruitbasket_seq RESTART WITH 2;
+ALTER SEQUENCE fruit_basket_seq RESTART WITH 2;
 
 INSERT INTO fruit(id, basket_id, name) VALUES (1, 1, 'Cherry');
 INSERT INTO fruit(id, basket_id, name) VALUES (2, 1, 'Apple');
